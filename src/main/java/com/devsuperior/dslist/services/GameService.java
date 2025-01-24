991c3439af7784cd90fcc3aadfef79440d3552a5
Eduardo Simonato <eduardo.simonato@pdcase.com.br>
2025-01-24 00:10:00 -0300

    Consulta SQL, projection, busca de games por lista

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,11 @@ public class GameService {
     public GameDTO findbyId( Long id ){
         Game result = gameRepository.findById(id).get();
         return  new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> searchByList(Long listId){
+        return gameRepository.searchByList(listId).stream().map( g -> new GameMinDTO(g) ).toList();
     }
 
 }
